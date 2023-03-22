2a4041cfc9db9321c110e5c2477cc4aae23564d6
二狗逗你玩 <815280716@qq.com>
2023-03-22 15:39:32 +0800

    chore: 添加vitepress构建文档

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,25 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
+const build = {
+  outDir: "lib",
+  lib: {
+    entry: path.resolve(__dirname, "./packages/index.ts"),
+    name: "MessUI",
+    fileName: "mess-ui",
+  },
+  rollupOptions: {
+    // 确保外部化处理那些你不想打包进库的依赖
+    external: ["vue"],
+    output: {
+      // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+      globals: {
+        vue: "Vue",
+      },
+    },
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -10,4 +29,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "examples"),
     },
   },
+  build,
 });
